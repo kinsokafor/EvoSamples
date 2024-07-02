569014f23d8dc372ed209083a1de821ff499c300
Kowebtech Ict <info@koweb.com.ng>
2024-07-02 12:29:10 +0100

    Update

---
+++ sample-template.blade.php
@@ -0,0 +1,26 @@
+<p>Index</p>
+{{\EvoPhp\Api\Operations::getIndex()}}
+
+<p>Access and create section model</p>
+@if(\EvoPhp\Api\Operations::checkAccess("13"))
+    @section('menu-members')
+    @show
+@endif
+
+<p>Render section model</p>
+@section('menu-members')
+
+@endsection
+
+<p>Go back button</p>
+<a href="javaScript:void(0)" id="goBackBtn">Go Back</a>
+
+<p>Content display</p>
+@section('content')
+    @yield('content') 
+@show
+
+<p>Links</p>
+{{$links['recoverPassword']}}
+{{$links['registration']}}
+{{$loginLink}}
