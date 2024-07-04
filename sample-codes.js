9e022246849b609ab156af37f395faf050974ca1
Kowebtech Ict <info@koweb.com.ng>
2024-07-04 21:08:13 +0100

    Update

--- sample-codes.js
+++ sample-codes.js
@@ -1,7 +1,17 @@
-export const toLocale = (number) => {
-    number = parseFloat(number)
-    number.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    });
-}
+export const toLocale = (str, prefix = "", suffix = "") => {
+    if(str == "" || str == undefined) str = 0;
+
+    function ordinaryString() {
+        var parts = (Math.round(str * 100) / 100).toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return prefix+parts.join(".")+suffix;
+    }
+
+    if(prefix != "" || suffix != "") return ordinaryString();
+    
+    str = parseFloat(str);
+    return str.toLocaleString("en-NG", {
+        style:"currency", 
+        currency:"NGN"
+    })
+} 
